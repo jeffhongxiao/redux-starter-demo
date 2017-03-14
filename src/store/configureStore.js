bda84e9458b853d2db491ad887a1d23226b68a78
hong <hong@hongs-iMac.local>
2017-03-14 20:27:42 -0300

    add immutable - part 2

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
@@ -6,6 +6,6 @@ export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableStateInvariant)
+    applyMiddleware(reduxImmutableStateInvariant())
   );
 }
