dca7b77dd10b3b57508f74844076abd214f42d07
hong <hong@hongs-iMac.local>
2017-03-14 21:37:34 -0300

    remove CREATE_COURSE

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,10 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course: course};
-}
-
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSE_SUCCESS, courses: courses};
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -2,12 +2,6 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
-      // use immutable
-      return [...state,
-        Object.assign({}, action.course)
-      ];
-
     case types.LOAD_COURSE_SUCCESS:
       return action.courses;
 
