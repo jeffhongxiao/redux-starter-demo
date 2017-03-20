4bbed3b66cd40d5aceacedd86f6763b578f9f2e3
hong <hong@hongs-iMac.local>
2017-03-20 10:39:33 -0300

    Create Save Course Thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,6 +5,14 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSE_SUCCESS, courses: courses};
 }
 
+export function updateCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course: course};
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course: course}
+}
+
 export function loadCourses() {
   return function(dispatch) {
     return courseApi.getAllCourses().then(courses => {
@@ -13,4 +21,15 @@ export function loadCourses() {
       throw(error);
     });
   };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => {
+      thorw(error);
+    });
+  }
 }
